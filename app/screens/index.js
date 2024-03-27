ff5bc3244a983f0314acdc159a4e5ad836e507c4
chintan0513 <chintantripathi1310@gmail.com>
2024-03-27 10:08:59 -0400

    initial setup

--- app/screens/index.js
+++
@@ -1,8 +0,0 @@
-import Login from './Login';
-import Signup from './Signup';
-import Welcome from './Welcome';
-import Questions from './Questions'
-import Landing from './Landing'
-
-export {Login, Signup, Welcome, Questions, Landing}
-
