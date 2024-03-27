6483f96c5040f2eb3bc56c15225a77f92ec8baf8
chintan0513 <chintantripathi1310@gmail.com>
2024-03-27 14:47:51 -0400

    Volunteer UI complete

--- client/app/screens/Questions.js
+++ client/app/screens/Questions.js
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet, TouchableOpacity, Image } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { firebase } from '../../config';
-import Animated from 'react-native-reanimated';
-import * as ImagePicker from 'expo-image-picker';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { firebase } from "../../config";
+import Animated from "react-native-reanimated";
+import * as ImagePicker from "expo-image-picker";
 
 const Questions = ({ navigation }) => {
-  const [income, setIncome] = useState('');
-  const [sin, setSin] = useState('');
+  const [income, setIncome] = useState("");
+  const [sin, setSin] = useState("");
   const [image, setImage] = useState(null);
   const [uploading, setUploading] = useState(false);
   const [isImageSelected, setIsImageSelected] = useState(false);
@@ -20,10 +29,10 @@ const Questions = ({ navigation }) => {
       setIsImageSelected(false);
     }
   }, [image]);
-  
+
   const formatSin = (input) => {
-    let formatted = input.replace(/\D/g, '');
-    formatted = formatted.replace(/(\d{3})(?=\d)/g, '$1-');
+    let formatted = input.replace(/\D/g, "");
+    formatted = formatted.replace(/(\d{3})(?=\d)/g, "$1-");
     return formatted;
   };
 
@@ -32,21 +41,22 @@ const Questions = ({ navigation }) => {
   };
 
   const pickImage = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (permissionResult.granted === false) {
-      Alert.alert('Permission to access camera roll is required!');
+      Alert.alert("Permission to access camera roll is required!");
       return;
     }
-    
+
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
-    
-    const source = { uri: result.assets[0].uri }
-  
+
+    const source = { uri: result.assets[0].uri };
+
     if (!result.cancelled) {
       setImage(source);
     }
@@ -54,85 +64,98 @@ const Questions = ({ navigation }) => {
 
   const handleSubmit = async () => {
     if (parseInt(income) > 10000) {
-        Alert.alert('Alert', 'Your income is too high to access the landing page');
+      Alert.alert(
+        "Alert",
+        "Your income is too high to access the landing page"
+      );
     } else if (!/^\d{3}-\d{3}-\d{3}$/.test(sin)) {
-        Alert.alert('Alert', 'Please enter a valid SIN number in the format ###-###-###');
+      Alert.alert(
+        "Alert",
+        "Please enter a valid SIN number in the format ###-###-###"
+      );
     } else {
-        try {
-            const usersRef = firebase.firestore().collection('SIN');
-            const currentUser = firebase.auth().currentUser;
+      try {
+        const usersRef = firebase.firestore().collection("SIN");
+        const currentUser = firebase.auth().currentUser;
 
-            if (currentUser) {
-                if (image) {
-                  setUploading(true);
-                  const response = await fetch(image.uri)
-                  const blob = response.blob()
-                  const filename = image.uri.substring(image.uri.lastIndexOf('/')+1)
-                  var ref = firebase.storage().ref().child(filename).put(blob)
-                  try {
-                      await ref;
-                  } catch (e){
-                      console.log(e)
-                  }
-                  setUploading(false)
-                  Alert.alert(
-                    'Photo uploaded!'
-                  );
-                  setImage(null);
-              }
-              
-                await usersRef.doc(currentUser.uid).set({
-                    sin: sin,
-                    income: parseInt(income),
-                    photo: image,
-                });
-
-                const serializableCurrentUser = {
-                    uid: currentUser.uid,
-                    email: currentUser.email,
-                    displayName: currentUser.displayName,
-                };
-
-                Alert.alert('Success', 'Your SIN, income, and photo have been stored');
-                navigation.navigate('Landing', { user: serializableCurrentUser });
+        if (currentUser) {
+          if (image) {
+            setUploading(true);
+            const response = await fetch(image.uri);
+            const blob = response.blob();
+            const filename = image.uri.substring(
+              image.uri.lastIndexOf("/") + 1
+            );
+            var ref = firebase.storage().ref().child(filename).put(blob);
+            try {
+              await ref;
+            } catch (e) {
+              console.log(e);
             }
-        } catch (error) {
-            console.error('Error storing SIN, income, and photo: ', error);
-            Alert.alert('Error', 'Failed to store SIN, income, and photo. Please try again later.');
+            setUploading(false);
+            Alert.alert("Photo uploaded!");
+            setImage(null);
+          }
+
+          await usersRef.doc(currentUser.uid).set({
+            sin: sin,
+            income: parseInt(income),
+            photo: image,
+          });
+
+          const serializableCurrentUser = {
+            uid: currentUser.uid,
+            email: currentUser.email,
+            displayName: currentUser.displayName,
+          };
+
+          Alert.alert(
+            "Success",
+            "Your SIN, income, and photo have been stored"
+          );
+          navigation.navigate("Landing", { user: serializableCurrentUser });
         }
+      } catch (error) {
+        console.error("Error storing SIN, income, and photo: ", error);
+        Alert.alert(
+          "Error",
+          "Failed to store SIN, income, and photo. Please try again later."
+        );
+      }
     }
-};
-  
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <KeyboardAwareScrollView
-        style={{ flex: 1, width: '100%' }}
-        keyboardShouldPersistTaps="always">
-
-        <Image source={require('../assets/background.png')} style={styles.backgroundImage} />
+        style={{ flex: 1, width: "100%" }}
+        keyboardShouldPersistTaps="always"
+      >
+        <Image
+          source={require("../assets/background.png")}
+          style={styles.backgroundImage}
+        />
 
         <View style={styles.logoContainer}>
           <Animated.Image
-            source={require('../assets/light.png')}
+            source={require("../assets/light.png")}
             style={styles.logoImage}
           />
           <Animated.Image
-            source={require('../assets/light.png')}
+            source={require("../assets/light.png")}
             style={styles.logoImage}
           />
         </View>
 
         <View style={styles.formContainer}>
-          <Animated.Text style={styles.title}>
-            Survey Form
-          </Animated.Text>
+          <Animated.Text style={styles.title}>Survey Form</Animated.Text>
 
           <View style={styles.inputContainer}>
             <Animated.View style={styles.inputView}>
               <TextInput
                 style={styles.inputText}
-                onChangeText={text => setIncome(text)}
+                onChangeText={(text) => setIncome(text)}
                 value={income}
                 keyboardType="numeric"
                 placeholder="Your Annual Income"
@@ -151,17 +174,37 @@ const Questions = ({ navigation }) => {
               />
             </Animated.View>
 
-            <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3" style={styles.selectButton} onPress={pickImage}>
-              <Text className="text-xl font-bold text-white text-center" style={styles.btnText}>Pick an Image</Text>
+            <TouchableOpacity
+              className="w-full bg-sky-400 p-3 rounded-2xl mb-3"
+              style={styles.selectButton}
+              onPress={pickImage}
+            >
+              <Text
+                className="text-xl font-bold text-white text-center"
+                style={styles.btnText}
+              >
+                Pick an Image
+              </Text>
             </TouchableOpacity>
             <View style={styles.imageContainer}>
-              {image && <Image source={{ uri: image.uri }} style={{ width: 50, height: 50 }} />}
+              {image && (
+                <Image
+                  source={{ uri: image.uri }}
+                  style={{ width: 50, height: 50 }}
+                />
+              )}
             </View>
 
             <TouchableOpacity
-              style={[styles.submitButton, !isImageSelected && { backgroundColor: '#ccc' }]}
-              onPress={handleSubmit}>
-              <Text style={[styles.submitText]} disabled={!isImageSelected}>Submit</Text>
+              style={[
+                styles.submitButton,
+                !isImageSelected && { backgroundColor: "#ccc" },
+              ]}
+              onPress={handleSubmit}
+            >
+              <Text style={[styles.submitText]} disabled={!isImageSelected}>
+                Submit
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -173,15 +216,15 @@ const Questions = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   backgroundImage: {
-    position: 'absolute',
-    height: '50%',
+    position: "absolute",
+    height: "50%",
   },
   logoContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     marginTop: 20,
   },
   logoImage: {
@@ -190,54 +233,55 @@ const styles = StyleSheet.create({
   },
   formContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     fontSize: 40,
     marginTop: 40,
-    fontWeight: 'bold',
-    color: 'rgb(56 189 248)',
+    fontWeight: "bold",
+    color: "rgb(56 189 248)",
     marginBottom: 20,
   },
   inputContainer: {
-    width: '80%',
+    width: "80%",
   },
   inputView: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
+    backgroundColor: "rgba(0,0,0,0.05)",
     borderRadius: 20,
     marginBottom: 20,
     paddingHorizontal: 20,
   },
   inputText: {
     height: 50,
-    color: '#1E3A8A',
+    color: "#1E3A8A",
   },
   inputMargin: {
     marginBottom: 30,
   },
   uploadButton: {
-    backgroundColor: '#1E3A8A',
+    backgroundColor: "#1E3A8A",
     borderRadius: 25,
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 20,
   },
   uploadText: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
   },
   submitButton: {
-    backgroundColor: '#1E3A8A',
+    backgroundColor: "rgb(56 189 248)",
+    marginTop: 15,
     borderRadius: 25,
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 20,
   },
   submitText: {
-    color: 'white',
+    color: "white",
     fontSize: 20,
   },
 });
--- client/app/screens/VolunteerScreen.js
+++ client/app/screens/VolunteerScreen.js
@@ -1,10 +1,101 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
 
-export default function VolunteerScreen() {
+const VolunteerScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState("false");
+
+  const handleSubmit = () => {
+    try {
+      setLoading(true);
+      if (!name || !email) {
+        Alert.alert("Please fill all fields");
+        setLoading(false);
+      }
+      console.log("Register Data==> ", { email, name });
+      setLoading(false);
+    } catch {
+      console.log("Error");
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Volunteer!</Text>
+    <View style={styles.container}>
+      <Text style={styles.pageTitle}>Volunteer Registration</Text>
+      <View style={{ marginVertical: 20 }}>
+        <Text style={{ color: "#fff" }}>Name</Text>
+        <TextInput
+          style={styles.inputBox}
+          autoCorrect={false}
+          keyboardType="Name"
+          autoCompleteType="name"
+          value={name}
+          onChangeText={setName}
+        />
+        <Text style={{ color: "#fff" }}>Email</Text>
+        <TextInput
+          style={styles.inputBox}
+          autoCorrect={false}
+          keyboardType="email-address"
+          autoCompleteType="email"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      {/* <Text>{JSON.stringify({name,email}, null, 4)}</Text> */}
+      <TouchableOpacity style={styles.registerBtn} onPress={handleSubmit}>
+        <Text style={styles.textBtn}>
+          {loading ? "Please Wait..." : "Register"}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+export default VolunteerScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2596be",
+  },
+  pageTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "#fff",
+  },
+  inputBox: {
+    height: 40,
+    width: 300,
+    marginBottom: 20,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingLeft: 10,
+  },
+  registerBtn: {
+    backgroundColor: "#000",
+    display: "flex",
+    padding: 10,
+    height: 40,
+    width: 300,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+  },
+  textBtn: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
